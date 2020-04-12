ca1821a1ef06a61ff76640c4c0b9d14937318a38
Taliesin <taliesin.bowes@hotmail.co.uk>
2020-04-12 18:44:42 +0100

    basic design

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
 <template>
   <div id="app">
-    <router-view />
+    <md-app md-waterfall md-mode="fixed">
+      <md-app-toolbar class="md-large md-dense md-primary">
+        <div class="md-toolbar-row">
+          <div class="md-toolbar-section-start">
+            <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+              <md-icon>menu</md-icon>
+            </md-button>
+              <span class="md-title">Nifty</span>
+              <md-tabs class="md-primary">
+              <md-tab id="tab-home" md-label="Home"></md-tab>
+              <md-tab id="tab-pages" md-label="Pages"></md-tab>
+              <md-tab id="tab-posts" md-label="Posts"></md-tab>
+              <md-tab id="tab-favorites" md-label="Favorites"></md-tab>
+            </md-tabs>
+          </div>
+        </div>
+      </md-app-toolbar>
+
+      <md-app-drawer :md-active.sync="menuVisible">
+        <md-toolbar class="md-transparent" md-elevation="0">
+          Navigation
+        </md-toolbar>
+      </md-app-drawer>
+
+      <md-app-content>
+         <router-view></router-view>
+      </md-app-content>
+    </md-app>
   </div>
 </template>
 
@@ -8,6 +35,11 @@
 
 export default {
   name: 'App',
+  data() {
+    return {
+      menuVisible: false,
+    }
+  },
   components: {
 
   }
@@ -15,12 +47,23 @@ export default {
 </script>
 
 <style>
+
+.md-app {
+  height: 100%;
+  font-family: 'Volkhov', serif;
+}
+
+.md-toolbar-row, .md-toolbar, .md-tabs-navigation {
+  min-height: 0 !important;
+  background-color: #2C3D50 !important;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  height: 100vh;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import '@/styles/style.css';
 import router from '@/router.ts';
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+import { MdApp, MdTabs, MdIcon, MdToolbar, MdButton, MdDrawer } from 'vue-material/dist/components'
+
+Vue.use(MdApp)
+Vue.use(MdTabs)
+Vue.use(MdIcon)
+Vue.use(MdToolbar)
+Vue.use(MdButton)
+Vue.use(MdDrawer)
 
 Vue.config.productionTip = false
 
