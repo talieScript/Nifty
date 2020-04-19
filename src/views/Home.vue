ba8bcd2439820ca3c99f21ab7bdd59f453d74dfa
Taliesin <taliesin.bowes@hotmail.co.uk>
2020-04-19 11:32:56 +0100

    Added Swiper by cdn

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,44 @@
 <template>
-    <div>
-        home
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">Slide 1</div>
+            <div class="swiper-slide">Slide 2</div>
+            <div class="swiper-slide">Slide 3</div>
+            <div class="swiper-slide">Slide 4</div>
+        </div>
+        <!-- Add Arrows -->
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
     </div>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
+    import Vue from 'vue';
+    // import Swiper from 'swiper';
 
     export default Vue.extend({
         name: 'Home',
+        data() {
+            return {
+                swiper: null,
+            }
+        },
+        mounted () {
+            this.swiper = new window.Swiper('.swiper-container', {
+                cssMode: true,
+                // Navigation arrows
+                navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+                },
+            });
+        },
     })
 </script>
 
 <style scoped>
-
+    .swiper-container {
+        width: 600px;
+        height: 300px;
+    }
 </style>
