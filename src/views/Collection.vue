ddd57369454bbda4f4cd2305e19b430b811348ac
Taliesin <taliesin.bowes@hotmail.co.uk>
2020-04-19 09:21:10 +0100

    collection page start

---
+++ src/views/Collection.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="collection">
+        {{collection}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Collection',
+        props: {
+            collection: {
+                type: Object,
+                requied: true,
+            },
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    .collection {
+        margin-top: 50px;
+    }
+</style>
