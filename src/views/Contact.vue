4fd9568589d038afd687d9ba8598655d58b4e3f5
Taliesin <taliesin.bowes@hotmail.co.uk>
2020-04-23 20:45:11 +0100

    completed contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -6,6 +6,20 @@
             :src="imageUrl"
         >
         <p v-html="parsedContent" class="content"></p>
+        <div class="details">
+            <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2515.3305341245564!2d-0.4539130485844393!3d50.91760236137425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4875bc0561fdbd47%3A0x814dd0fee0b0a98c!2sNifty%20Art%20Gallery!5e0!3m2!1sen!2suk!4v1587668463475!5m2!1sen!2suk" width="600" height="450" frameborder="0" style="border:0;"  allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <div class="info">
+                <div class="detail">
+                    <md-icon color="blue">location_on</md-icon><p>{{ address }}</p>
+                </div>
+                <div class="detail">
+                    <md-icon color="blue">phone</md-icon><p>{{ phone }}</p>
+                </div>
+                <div class="detail">
+                    <md-icon color="blue">email</md-icon><p>{{ email }}</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -17,10 +31,19 @@
 
     export default Vue.extend({
         name: 'Contact',
+        props: {
+            windowWidth: {
+                type: Number,
+                required: true,
+            },
+        },
         data() {
             return {
                 content: '',
                 imageUrl: '',
+                address: '',
+                phone: '',
+                email: '',
             }
         },
         created() {
@@ -29,6 +52,9 @@
                     this.content = data.content;
                     console.log(data)
                     this.imageUrl = getPicUrl(data.Picture.url)
+                    this.address = data.Address;
+                    this.phone = data.Phone;
+                    this.email = data.Email;
                 })
         },
         computed: {
@@ -61,6 +87,9 @@
         padding: 16px;
         text-align: left;
         background: #F0F3F4;
+        @media only screen and (max-width: 56.25em) {
+            padding: 10px;
+        }
     }
     .gallery-image {
         box-shadow: -1px 4px 5px 1px rgba(0,0,0,0.30);
@@ -70,10 +99,18 @@
         float: right;
         height: auto;
         border-radius: 5px;
+        @media only screen and (max-width: 56.25em) {
+            display: none;
+            margin: 30px 15px 0px 10px;
+            width: 35vw;
+        }
     }
     .h1 {
         margin: 0;
         padding: 30px 0px 0px 20px;
+        @media only screen and (max-width: 56.25em) {
+            padding: 15px 0px 0px 10px;
+        }
         font-size: 35px;
     }
     .content {
@@ -82,8 +119,42 @@
         font-size: 20px;
         line-height: 25px;
        @media only screen and (max-width: 56.25em) {
-           font-size: 15px;
+            font-size: 15px;
             line-height: unset;
+            padding: 15px
         };
+    }
+    .details {
+        display: flex;
+        p {
+            margin: 0;
+            margin-left: 10px;
+        }
+        @media only screen and (max-width: 56.25em) {
+            flex-direction: column-reverse;
+            align-items: center;
+            justify-content: space-between;
+        }
+    }
+    .info {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: flex-start;
+        min-height: 105px;
+        @media only screen and (max-width: 56.25em) {
+            height: 15vh;
+            margin-bottom: 20px;
+        }
+    }
+    .detail {
+        display: flex;
+        i {
+            color: #2F89A0!important;
+            margin: 0;
+        }
+    }
+    .map {
+        margin: 0 30px;
     }
 </style>
