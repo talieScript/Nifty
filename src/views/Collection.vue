654b9ab08f989f6fc38e045a6171238e6b211e7c
Taliesin <taliesin.bowes@hotmail.co.uk>
2020-04-24 23:53:17 +0100

    added caption

--- src/views/Collection.vue
+++ src/views/Collection.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="collection">
-        <div v-masonry="containerId" fit-width stamp=".desc" :collumn-width="20" :gutter="10" class="picture-container" transition-duration="0.3s" item-selector=".item">
+        <div v-masonry fit-width stamp=".desc" :collumn-width="20" :gutter="10" class="container" transition-duration="0.3s" item-selector=".item">
             <div class="desc-container">
                 <h2 class="title">{{collection.Title}}</h2>
                 <p class="desc" v-html="collection.Description"></p>
             </div>
-            <div v-masonry-tile class="item" v-for="(picture, index) in collection.pictures" :key="index">
+            <div v-masonry-tile class="image-container item" v-for="(picture, index) in collection.pictures" :key="index">
                 <img
                     class="image"
                     :src="getPicUrl(picture.Image.url)"
                 >
+                <p class="caption">
+                    {{ picture.Title }}
+                </p>
             </div>
         </div>
     </div>
@@ -59,20 +62,25 @@
         font-size: 18px;
         line-height: 23px
     }
-    .picture-container {
+    .container {
         padding-left: 15px;
         @media only screen and (max-width: 75em) {
             padding-left: 7px;
         }
     }
-    .image {
-        box-shadow: -1px 4px 5px 1px rgba(0,0,0,0.30);
-        max-height: 90%;
-        border-radius: 5px;
-        width: 24vw;
+    .image-container {
         margin-bottom: 10px;
+        width: 24vw;
+        position: absolute;
         @media only screen and (max-width: 75em) {
             width: 32vw;
         };
+    }
+    .image {
+        box-shadow: -1px 4px 5px 1px rgba(0,0,0,0.30);        border-radius: 5px;
+        width: 100%;
+    }
+    .caption {
+        text-align: center;
     }
 </style>
