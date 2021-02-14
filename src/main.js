5c5a6729e88f98ca836975a9c7abde168def1264
Taliesin <taliesin.bowes@hotmail.co.uk>
2021-02-14 17:23:20 +0000

    added google analytics

--- src/main.js
+++ src/main.js
@@ -7,7 +7,13 @@ import { MdApp, MdTabs, MdIcon, MdToolbar, MdButton, MdDrawer, MdContent, MdList
 import vSelect from 'vue-select';
 import VueMeta from 'vue-meta';
 import store from './store/store.ts'
+import VueAnalytics from 'vue-analytics';
 
+
+Vue.use(VueAnalytics, {
+  id: process.env.VUE_APP_GOOGLE_ANALYTICS,
+  router
+})
 
 Vue.use(VueMeta, { refreshOnceOnNavigation: true })
 
